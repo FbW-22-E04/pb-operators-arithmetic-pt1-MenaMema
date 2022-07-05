4e5fe089e748b3c46ebac424c61bff303324d07f
MenaMema <amenabarrera@gmail.com>
2022-07-05 17:00:17 +0200

    almost done

--- solution.js
+++ solution.js
@@ -1,1 +1,50 @@
 // write your code in this file
+
+//Question1
+
+const total = 6450 / 15;
+console.log("Q1:", total, "tickets are sold");
+
+//Question2
+
+console.log("Q2:", 500 * 52);
+
+//Percentage
+//Question3
+
+console.log("Q3:", 0.17 * 30);
+
+//Geometry Formulas
+//Question4
+
+console.log("Q4:", 4.75 * 4, "cm");
+
+//Question5
+
+console.log("Q5:", 5 + 6 + 7, "cm");
+
+//Question6
+
+console.log("Q6:", 4 * 5, "cm");
+
+//Question7
+
+console.log("Q7:", (3 * 4) / 5);
+
+//Question8
+
+console.log("Q8:", 5 * 5 * 5);
+
+//Consumer Formula
+//Question9
+
+console.log("Q9:", (22.35 * 10) / 100, (26.67 * 15) / 100, (35.92 * 20) / 100);
+
+//Average
+//Question10
+
+console.log(
+  "Q10:",
+  (480 + 360 + 300 + 540 + 480 + 120 + 60 + 510 + 420 + 240) / 10 / 60,
+  "hours per day"
+);
